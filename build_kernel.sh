18a8e1143ec43e518956f96ae889fb3c97af3589
Azmathulla Shaik <107968141+IamAzmathullaShaikh@users.noreply.github.com>
2024-07-03 11:13:53 +0530

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,5 +5,5 @@ export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=t
 export ARCH=arm64
 
-make ARCH=arm64 a12s_defconfig
+make ARCH=arm64 lineage_a12s_defconfig
 make ARCH=arm64 -j$(nproc --all)
